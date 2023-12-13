aca7c1d5a3b7269a05467dace88caaf28afeb2cf
Omar Mohamed <64428610+skittoo@users.noreply.github.com>
2023-12-13 18:47:54 +0200

    fixing the bug

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-app.route('/')
+@app.route('/')
 def index():
     return "<h1> Hello Mety </h1>"
 
 
 if __name__ == '__main__':
-    app.run()
-    
+    app.run(debug=True, port="5003")
